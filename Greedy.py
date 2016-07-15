3ca3aef77274e075019dc962296b72d2779cb26a
Jatin <jatin4247@gmail.com>
2016-07-15 19:10:47 +0530

    new file:   .Greedy.py.swm 	new file:   .Greedy.py.swp 	new file:   .cheat_sheet.swp 	new file:   .singelton.py.swo 	modified:   Greedy.py 	new file:   __pycache__/singelton.cpython-35.pyc 	new file:   cheat_sheet
    Note, all the swm and swp files are auto-generated due to stuff Vim does
    and I'm actually not sure what the __pycache__ is for...

--- Greedy.py
+++ Greedy.py
@@ -1,31 +1,62 @@
 #!/usr/bin/env python
 
-from ABC import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod
 from singelton import Singelton
+
+'''
+    This section just has the required classes I want for this file
+    It contains an abstract class called Node which should never be
+    instantiated (I dunno if that's the correct spelling) because it
+    won't tell if any given node is a Root Node or a Sub-Node.
+'''
+
 
 class Node(object, metaclass=ABCMeta):
     @abstractmethod
     def __init__(self):
         self.value = None
 
-@Singelton
+@Singelton # The root node is a Singelton for obvious reasons
 class Root(Node):
     def __init__(self):
-        Node.__init__()
-
+        Node.__init__(self)
+#And now just a simple node class
 class SubNode(Node):
     def __init__(self):
-        Node.__init__()
+        Node.__init__(self)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Functions
 
 def GenTree(depth, base):
+    '''
+        Function to generate a tree structure with any give base
+        It will create the required number of nodes at any depth,
+        and then return those nodes (which are just instances of the class
+        SubNode() defined up above) and also it will return an instance of
+        this function with the depth subtracted. So we should have stop
+        once we reach the root node
+    '''
+    nodes_at_depth = {}
     if depth == 0:
         y = Root()
         return y
     else:
+        nodes_lst = []
         nodes = (depth)*base
-        for i in xrange(nodes):
-            nodes_at_depth = {}
+        for i in range(depth):
+            nodes_lst.append(i)
+        for i in range(0, nodes):
             nodes_at_depth[str(depth)+'_instance_'+str(i)] = SubNode()
-        return GenTree(depth-1, base)
+        return [GenTree(depth-1, base), nodes_at_depth]
+
+f = GenTree(3,2)
+'''
+    From the following statement I do get a bunch of Instances of
+    SubNode() but not the way I want them to generate
+'''
+# print(f)
+'''
+    The following line should print out the second instance of
+    the second, layer maybe? But it just says key error
+'''
+# print(f[1]['2_instance_2'])
